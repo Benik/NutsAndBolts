505fc32bed001e95b1888e59b644c630c0ab7902
Benik <benik@otenet.gr>
2018-09-21 21:19:13 +0300

    Added Location Lite locales

--- ElvUI_NutsAndBolts/locales/enUS.lua
+++ ElvUI_NutsAndBolts/locales/enUS.lua
@@ -4,8 +4,43 @@ local L = AceLocale:NewLocale('ElvUI', 'enUS');
 if not L then return; end
 
 L["%s version %s by Benik"] = true
-L["%s is a collection of my plugins in Tukui.org / Twitch"]
-L["Information / Help"]
-L["Please use the following links if you need help or wish to know more about this AddOn."]
-L["AddOn Description"]
-L["Report Bugs / Request more Nuts & Bolts"]L["Report Bugs or Request more Nuts & Bolts"]
+L["%s is a collection of my plugins in Tukui.org / Twitch"] = true
+L["Information / Help"] = true
+L["Please use the following links if you need help or wish to know more about this AddOn."] = true
+L["AddOn Description"] = true
+L["Report Bugs / Request more Nuts & Bolts"]L["Report Bugs or Request more Nuts & Bolts"] = true
+
+-- Location Lite
+L[".:: LocationPlus - LocationLite ::.\nIt's not wise to use both Plus and Lite versions."] = true
+L["Adds 2 digits in the coords"] = true
+L["Adds 6 pixels at the Main Location Panel height."] = true
+L["Adjust All Panels Height."] = true
+L["Adjust coords updates (in seconds) to avoid cpu load. Bigger number = less cpu load. Requires reloadUI."] = true
+L["Adjust the Location Panel Width."] = true
+L["All Panels Height"] = true
+L["AsphyxiaUI Style"] = true
+L["Auto Colorize"] = true
+L["Auto resized Location Panel."] = true
+L["Auto width"] = true
+L["Choose font for the Location and Coords panels."] = true
+L["Coordinates"] = true
+L["Detailed Coords"] = true
+L["Displays the main zone and the subzone in the location panel"] = true
+L["Enable/Disable layout with shadows."] = true
+L["Enable/Disable transparent layout."] = true
+L["Hide Blizzard Zone Text"] = true
+L["Hides all panels background so you can place them on ElvUI's top or bottom panel."] = true
+L["Larger Location Panel"] = true
+L["Location Lite"] = true
+L["Location Panel"] = true
+L["Location Plus"] = true
+L["LocationLite adds a movable player location panel"] = true
+L["Set the font size."] = true
+L["Shadows"] = true
+L["Show/Hide all panels when in combat"] = true
+L["Truncate text"] = true
+L["Truncates the text rather than auto enlarge the location panel when the text is bigger than the panel."] = true
+L["Update Timer"] = true
+L["Use Custom Location Color"] = true
+L["Zone and Subzone"] = true
+L["\nPlease choose which one to disable."] = true
