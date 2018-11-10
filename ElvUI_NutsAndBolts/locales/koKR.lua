49b9b3e97bc75264b4405f7629f0006614f466c5
Benik <benik@otenet.gr>
2018-11-10 21:55:38 +0200

    Updated all locale files

--- ElvUI_NutsAndBolts/locales/koKR.lua
+++ ElvUI_NutsAndBolts/locales/koKR.lua
@@ -1,3 +1,71 @@
 ﻿local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
 local L = AceLocale:NewLocale("ElvUI", "koKR")
 if not L then return end
+
+L["%s version %s by Benik"] = true
+L["%s is a collection of my plugins in Tukui.org / Twitch"] = true
+L["Information / Help"] = true
+L["Please use the following links if you need help or wish to know more about this AddOn."] = true
+L["AddOn Description"] = true
+L["Report Bugs / Request more Nuts & Bolts"] = true
+
+-- Location Lite
+L["Adds 2 digits in the coords"] = true
+L["Adds 6 pixels at the Main Location Panel height."] = true
+L["Adjust All Panels Height."] = true
+L["Adjust coords updates (in seconds) to avoid cpu load. Bigger number = less cpu load. Requires reloadUI."] = true
+L["Adjust the Location Panel Width."] = true
+L["All Panels Height"] = true
+L["AsphyxiaUI Style"] = true
+L["Auto Colorize"] = true
+L["Auto resized Location Panel."] = true
+L["Auto width"] = true
+L["BenikUI Style"] = true
+L["Choose font for the Location and Coords panels."] = true
+L["Coordinates"] = true
+L["Detailed Coords"] = true
+L["Displays the main zone and the subzone in the location panel"] = true
+L["Enable/Disable layout with shadows."] = true
+L["Enable/Disable transparent layout."] = true
+L["Hide Blizzard Zone Text"] = true
+L["Hides all panels background so you can place them on ElvUI's top or bottom panel."] = true
+L["It looks like %s is loaded. If you would like to disable it, then click on the button below."] = true
+L["Larger Location Panel"] = true
+L["Location Lite"] = true
+L["Location Panel"] = true
+L["Location Plus"] = true
+L["LocationLite adds a movable player location panel"] = true
+L["|cff00c0faNOTE:|r The standalone Location Lite is not gonna be available anymore either on Twitch or Tukui.org."] = true
+L["Set the font size."] = true
+L["Shadows"] = true
+L["Show/Hide all panels when in combat"] = true
+L["Truncate text"] = true
+L["Truncates the text rather than auto enlarge the location panel when the text is bigger than the panel."] = true
+L["Update Timer"] = true
+L["Use Custom Location Color"] = true
+L["Zone and Subzone"] = true
+
+-- Databar Colors
+L['DataBar Colors'] = true
+L["This module can alter ElvUI Databar statusbars to any color"] = true
+
+-- ElvUI Panels
+L['ElvUI Panels'] = true
+L["This module can alter ElvUI Top and Bottom Panels Transparency and Height"] = true
+
+-- DataText Colors
+L['DataText Color'] = true
+L["Are you sure you want to disable ElvUI_DTColors?"] = true
+L["This module can alter all ElvUI DataText text color"] = true
+L["It looks like ElvUI_DatatextColors addon is loaded. It is strongly suggested to disable it, by clicking the button below.\n|cff00c0faNOTE:|r The standalone ElvUI_DatatextColors addon is not gonna be available anymore on Tukui.org."] = true
+
+-- BagsCountPosition
+L['Bags Count Position'] = true
+L["This module can move the Bag Item Count Position"] = true
+
+-- ObjectiveTracker
+L['Objective Tracker'] = true
+L["This module can toggle Objective Tracker when in an instance"] = true
+L["Hide when in an instance"] = true
+L["Don't hide on Mythic+"] = true
+L["Don't hide when an achievement is tracked"] = true
